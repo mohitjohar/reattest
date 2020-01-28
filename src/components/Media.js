e11d14d63c0ae156a8bb08427daded00df5da391
mohitjohar <mkjcsoftwaresolution@gmail.com>
2020-01-29 00:33:11 +0530

    push code

--- src/components/Media.js
+++ src/components/Media.js
@@ -3,25 +3,24 @@ import { Progress } from 'reactstrap';
 import Pathimg from '../img/icon/Path.png';
 import heartimg from '../img/icon/heart.png';
 import cloudimg from '../img/icon/cloud.png';
-import img from '../img/icon/img.png';
 
-const Media = () => {
+const Media = ({ img, h5, h3, heart, notifi, progress1, text }) => {
   return (
     <div className="media">
       <img src={img} className="img" alt="" />
       <div className="content">
-        <h5>JoeRogan</h5>
-        <h3>JRE MMA Show #75 wit</h3>
-        <p className="mb-2">Joe is joined by for</p>
+        <h5>{h5}</h5>
+        <h3>{h3}</h3>
+        <p className="mb-2">{text}</p>
         <div className="d-flex align-items-center justify-content-between">
           <ul className="imgflex">
             <li className="d-flex">
               <img src={heartimg} alt="" />
-              <span>84</span>
+              <span>{heart}</span>
             </li>
             <li className="d-flex">
               <img src={Pathimg} alt="" />
-              <span>96</span>
+              <span>{notifi}</span>
             </li>
             <li className="d-flex">
               <img src={cloudimg} alt="" />
@@ -29,7 +28,7 @@ const Media = () => {
           </ul>
           <div className="progress-wrapper">
             <Progress color="info" value={50} />
-            <span>97%</span>
+            <span>{progress1}%</span>
           </div>
         </div>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,38 @@
 import React, { useState } from 'react';
 import Header from '../components/header/Header';
+import img from '../img/icon/img.png';
+import img1 from '../img/icon/img1.png';
 import Media from '../components/Media';
+
+const playlists = [
+  {
+    img,
+    h5: 'JoeRogan',
+    h3: 'JRE MMA Show #75 wit',
+    text: 'Joe is joined by for',
+    heart: 84,
+    notifi: 96,
+    progress: 48
+  },
+  {
+    img: img1,
+    h5: 'myfavoritemurder',
+    h3: 'MFM Minisode 13',
+    text: 'This week’s hometown',
+    heart: 43,
+    notifi: 87,
+    progress: 79
+  },
+  {
+    img,
+    h5: 'JoeRogan',
+    h3: 'JRE MMA Show #75 wit',
+    text: 'This week’s hometown',
+    heart: 84,
+    notifi: 96,
+    progress: 48
+  }
+];
 
 const Home = () => {
   return (
@@ -9,12 +41,22 @@ const Home = () => {
       <div className="container pt-3 pb-3">
         <h2>Playlists</h2>
         <div className="media-wrapper">
-          <Media />
-          <hr className="divider" />
-          <Media />
-          <hr className="divider" />
-          <Media />
-          <hr className="divider" />
+          {playlists.map(list => {
+            return (
+              <>
+                <Media
+                  img={list.img}
+                  h5={list.h5}
+                  h3={list.h3}
+                  text={list.text}
+                  heart={list.heart}
+                  notifi={list.notifi}
+                  progress1={list.progress}
+                />
+                <hr className="divider" />
+              </>
+            );
+          })}
         </div>
       </div>
     </div>
